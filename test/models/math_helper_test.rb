53837b170443baa3fea76f7ed585558233560637
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-17 15:53:29 +0000

    TDD step 3: added edge cases test, division by 0 test breaks our function!!! we found a bug!

--- test/models/math_helper_test.rb
+++ test/models/math_helper_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 class MathHelperTest < ActiveSupport::TestCase
-  test "divide numbers" do
+  test "divide positive numbers" do
     assert_equal 2, MathHelper.divide(6, 3)
+  end
+
+  test "divide by zero returns nil" do
+    assert_nil MathHelper.divide(5, 0)
+  end
+
+  test "divide negative and positive" do
+    assert_equal(-4, MathHelper.divide(-8, 2))
   end
 end
