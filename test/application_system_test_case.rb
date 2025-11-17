731adbb2547fe86cce019bb9041c269dd28fd42c
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-17 15:06:26 +0000

    setup webmocks for stubs

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,4 +1,7 @@
 require "test_helper"
+require "webmock/minitest"
+
+WebMock.disable_net_connect!(allow_localhost: true)
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
