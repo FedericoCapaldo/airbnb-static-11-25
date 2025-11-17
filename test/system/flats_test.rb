27d57424fcf97205b7936fdff019e31f3108a3e2
Federico Capaldo <federico.capaldo@hotmail.com>
2025-11-17 15:13:13 +0000

    TDD step 1: added a complex system test with stub data (when making tests, your test MUST be independent from other systems so we stub data, aka provide expected fake data when calling a function like an API call

--- test/system/flats_test.rb
+++ test/system/flats_test.rb
@@ -7,9 +7,34 @@ class FlatsTest < ApplicationSystemTestCase
   #   assert_selector "h1", text: "Flats"
   # end
 
-  test "visiting flats page" do
+  test "visiting flats page shows 2 flats" do
+    fake_json = [
+      {
+        id: 145,
+        name: "Charm at the Steps of the Sacre Coeur/Montmartre",
+        imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+        price: 164,
+        priceCurrency: "EUR"
+      },
+      {
+        id: 148,
+        name: "Trendy Apt in Buttes Montmartre",
+        imageUrl: "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+        price: 200,
+        priceCurrency: "EUR"
+      }
+    ].to_json
+
+    stub_request(:get, "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json")
+      .to_return(status: 200, body: fake_json, headers: { "Content-Type" => "application/json" })
+
+
     visit "/"
 
     assert_selector "h1", text: "Flats"
+    assert_selector "li", count: 2
+
+    assert_text "Charm at the Steps of the Sacre Coeur/Montmartre"
+    assert_text "Trendy Apt in Buttes Montmartre"
   end
 end
